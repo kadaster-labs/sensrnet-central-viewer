c3500f0a6e150ea860429b60880afb563e318709
Stefan Bussemaker <Stefan.Bussemaker@kadaster.nl>
2021-03-24 15:42:22 +0100

    change sensorType enum to string

--- src/app/model/sensor.ts
+++ src/app/model/sensor.ts
@@ -1,34 +1,12 @@
 import { Device } from './device';
 
-export enum SensorType {
-  NavigationBeacon = 'NavigationBeacon',
-  EnvironmentalZoneCameras = 'EnvironmentalZoneCameras',
-  SecurityCamera = 'SecurityCamera',
-  TraphicFlashlight = 'TraphicFlashlight',
-  WeatherStation = 'WeatherStation',
-  WindGauge = 'WindGauge',
-  WasteContainers = 'WasteContainers',
-  FineDustSensor = 'FineDustSensor',
-  UVSensor = 'UVSensor',
-  LightCell = 'LightCell',
-  MotionSensor = 'MotionSensor',
-  FireDetector = 'FireDetector',
-  WaterLevelMeter = 'WaterLevelMeter',
-  MicrophoneOrSoundMeter = 'MicrophoneOrSoundMeter',
-  PedometerCounters = 'PedometerCounters',
-  RadarDetector = 'RadarDetector',
-  GMSSensor = 'GMSSensor',
-  DetectionLoop = 'DetectionLoop',
-  HeightDetectionDevice = 'HeightDetectionDevice'
-}
-
 export interface Sensor {
   _id: string;
 
   name: string;
   description: string;
 
-  type: SensorType;
+  type: string;
   manufacturer?: string;
   supplier?: string;
   documentation?: string;
